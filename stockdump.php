dbe4b75e1f6e717930308c88127bc3fbbc9275f1
Optykan <syang0525@gmail.com>
2015-09-30 13:29:53 -0400

    static stock framework

--- stockdump.php
+++ stockdump.php
@@ -1,4 +1,6 @@
+<!DOCTYPE HTML>
 <?php
+//ichart.yahoo.com/table.csv?s=BAS.DE&a=0&b=1&c=2015&d=08&e=27&f=2015&g=w&ignore=.csv
 $date = date_create (date("Y-m-d"));
 date_sub($date,date_interval_create_from_date_string("40 days"));
 $date= date_format($date,"Y-m-d");
@@ -9,16 +11,64 @@ $year = $date[0];
 $nday = date("d");
 $nmonth = date("m")-1;
 $nyear = date("Y");
-echo $day.'-'.$month.'-'.$year."</br>";
-echo $nday.'-'.$nmonth.'-'.$nyear."</br>";
     
-$stockdata = file_get_contents('http://ichart.yahoo.com/table.csv?s=%5EGSPTSE&a='.$month.'&b='.$day.'&c='.$year.'&d=.'.$nmonth.'.&e='.$nday.'&f='.$nyear.'&g=d&ignore=.csv');
+$stockdata = file_get_contents('http://ichart.yahoo.com/table.csv?s=BRK-A&a='.$month.'&b='.$day.'&c='.$year.'&d=.'.$nmonth.'.&e='.$nday.'&f='.$nyear.'&g=d&ignore=.csv');
 $stocks = explode ("\n", $stockdata);
 //newlines get exploded into array
 for ($i = 1; $i<50; $i++){
     $stocks[$i] = explode (",", $stocks[$i]);
+    $stocks[$i][0] = explode ("-",  $stocks[$i][0]);
     if ($stocks[$i][6] != 0) {
-    echo number_format((float)$stocks[$i][6], 2, '.', '')."</br>";
+    //echo number_format((float)$stocks[$i][6], 2, '.', '')."</br>";
     }
 }
 ?>
+    <html>
+
+    <head>
+        <script type="text/javascript">
+            window.onload = function () {
+                var chart = new CanvasJS.Chart("chartContainer", {
+                    title: {
+                        text: "BRK-A Composite Index",
+                    },
+                    exportEnabled: true,
+                    axisY: {
+                        includeZero: false,
+                        prefix: "$",
+                        title: "Prices",
+                    },
+                    axisX: {
+                        interval: 1,
+                        valueFormatString: "MMM-DD",
+                    },
+                    toolTip: {
+                        content: "Date:{x}</br><strong>Prices:</strong></br>Open:{y[0]}, Close:{y[3]}</br>High:{y[1]},Low:{y[2]}",
+                    },
+                    data: [
+                        {
+                            type: "candlestick",
+                            dataPoints: [ // Y: [Open, High ,Low, Close]
+            <?php
+for ($i=1; $i<50; $i++){
+    if ($stocks[$i][0][0] != 0){
+        $m = $stocks[$i][0][1]-1;
+			echo "{x:  new Date(".$stocks[$i][0][0],",".$m.",".$stocks[$i][0][2]."),  y:[".$stocks[$i][1].",".$stocks[$i][2].",".$stocks[$i][3].",".$stocks[$i][4]."]},"."\n";
+    }}?>
+		]
+	}
+	]
+                });
+                chart.render();
+            }
+        </script>
+        <script type="text/javascript" src="js/canvasjs.min.js"></script>
+    </head>
+
+    <body>
+        <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+        <?='http://ichart.yahoo.com/table.csv?s=BRK-A&a='.$month.'&b='.$day.'&c='.$year.'&d=.'.$nmonth.'.&e='.$nday.'&f='.$nyear.'&g=d&ignore=.csv'?>
+    </body>
+
+
+    </html>
--- stocks.php
+++ stocks.php
@@ -12,11 +12,12 @@ $nday = date("d");
 $nmonth = date("m")-1;
 $nyear = date("Y");
     
-$stockdata = file_get_contents('http://ichart.yahoo.com/table.csv?s=%5EGSPTSE&a='.$month.'&b='.$day.'&c='.$year.'&d=.'.$nmonth.'.&e='.$nday.'&f='.$nyear.'&g=d&ignore=.csv');
+$stockdata = file_get_contents('http://ichart.yahoo.com/table.csv?s=BRK-A&a='.$month.'&b='.$day.'&c='.$year.'&d=.'.$nmonth.'.&e='.$nday.'&f='.$nyear.'&g=d&ignore=.csv');
 $stocks = explode ("\n", $stockdata);
 //newlines get exploded into array
 for ($i = 1; $i<50; $i++){
     $stocks[$i] = explode (",", $stocks[$i]);
+    $stocks[$i][0] = explode ("-",  $stocks[$i][0]);
     if ($stocks[$i][6] != 0) {
     //echo number_format((float)$stocks[$i][6], 2, '.', '')."</br>";
     }
@@ -81,7 +82,7 @@ for ($i = 1; $i<50; $i++){
             </aside>
             <section class="content">
                 <div class="col-xs-12">
-                    <canvas id="tsx" width="1280" height="800"></canvas>
+                    <div id="chartContainer" style="height: 400px; width: 100%;"></div>
 
                 </div>
             </section>
@@ -92,36 +93,45 @@ for ($i = 1; $i<50; $i++){
         <script src="js/metisMenu.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js"></script>
         <script src="js/chart.min.js"></script>
-
-
-        <script>
-            // line chart data
-            var stockData = {
-                    labels: [<?php 
-                        for ($i = 1; $i < 50; $i++) {
-                            if ($stocks[$i][0] != ""){
-                                echo '"'.$stocks[$i][0].'",';}
-                        } ?>
-                        ],
-                    datasets : [
-                                {
-                                    fillColor: "rgba(172,194,132,0.4)",
-                                    strokeColor: "#ACC26D",
-                                    pointColor: "#fff",
-                                    pointStrokeColor: "#9DB86D",
-                                    data: [<?php for ($i = 1; $i < 50; $i++) {
-                                if ($stocks[$i][6] !=0) {
-                                    echo number_format((float)$stocks[$i][6], 2, '.', '').",";
-                                }
-                        }?>]
-                }
-            ]
+        <script type="text/javascript" src="js/canvasjs.min.js"></script>s
+        <script type="text/javascript">
+            window.onload = function () {
+                var chart = new CanvasJS.Chart("chartContainer", {
+                    title: {
+                        text: "BRK-A Composite Index",
+                    },
+                    exportEnabled: true,
+                    axisY: {
+                        includeZero: false,
+                        prefix: "$",
+                        title: "Prices",
+                    },
+                    axisX: {
+                        interval: 1,
+                        valueFormatString: "MMM-DD",
+                    },
+                    toolTip: {
+                        content: "Date:{x}</br><strong>Prices:</strong></br>Open:{y[0]}, Close:{y[3]}</br>High:{y[1]},Low:{y[2]}",
+                    },
+                    data: [
+                        {
+                            type: "candlestick",
+                            dataPoints: [ // Y: [Open, High ,Low, Close]
+                                <?php
+                    for ($i=1; $i<50; $i++){
+                        if ($stocks[$i][0][0] != 0){
+                            $m = $stocks[$i][0][1]-1;
+                                echo "{x:  new Date(".$stocks[$i][0][0],",".$m.",".$stocks[$i][0][2]."),  y:[".$stocks[$i][1].",".$stocks[$i][2].",".$stocks[$i][3].",".$stocks[$i][4]."]},"."\n";
+                        }}?>
+                            ]
                         }
-                        // get line chart canvas
-                    var tsx = document.getElementById('tsx').getContext('2d');
-                    // draw line chart
-                    new Chart(tsx).Line(stockData);
+                    ]
+                });
+                chart.render();
+            }
         </script>
+
+
         <script>
             $(function () {
                 $('#menu').metisMenu();
